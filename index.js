b2a6c92211fbd4b3dcdd055da144b765a253d2bf
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-21 12:20:18 -0800

    Clock Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+$(document).ready(function() {
+  console.log("You are cool and the document has loaded");
+
+  var timeNow = function(){
+    fullDate = new Date (Date.now());
+    return fullDate.toLocaleTimeString();
+  };
+
+  // var intervalClock = setInterval(timeNow, 1000);
+  // var clockAsString = timeNow().toString();
+  var incrementTime = function(){
+    var time = $('<h3>' + timeNow() + '</h3>');
+    $('#clock').html(time);
+  };
+
+  setInterval(function() {incrementTime();}, 1000);
+
+
+});
